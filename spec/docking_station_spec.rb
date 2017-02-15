57d7a57a5ac1d37d6392a43bdc4042e7e1fd8436
Alex Imm <alexanderimm@hotmail.co.uk>
2017-02-15 17:04:01 +0000

    added docking bikes and seeing what bikes are in the dock

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -17,4 +17,10 @@ describe DockingStation do
   it 'contains a bike' do
     expect(subject).to respond_to (:bike)
   end
+
+  it 'returns docked bike' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
+  end
 end
