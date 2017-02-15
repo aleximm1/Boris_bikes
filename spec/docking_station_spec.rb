2d98f08bf044898fc35e8c47c3f81fd02abd32e3
Alex Imm <alexanderimm@hotmail.co.uk>
2017-02-15 13:50:12 +0000

    added test for getting a bike

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,4 +4,7 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
     # expect(subject).to respond_to :release_bike
   # end
+  it 'get a bike' do
+    expect(subject.release_bike).to be_a Bike
+  end
 end
