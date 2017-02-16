e85ff42260c8b558c9fe173df227ecfd50c8d5cc
Alex Imm <alexanderimm@hotmail.co.uk>
2017-02-16 19:54:16 +0000

    challenge 15 done

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,26 +1,37 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike, :bike_counter
+  attr_reader :bikes
 
   def initialize
-    @bike_counter = true
+    @bikes = []
   end
 
   def release_bike
-    if bike_counter == true
-      @bike = Bike.new
-      @bike_counter = false
-      return @bike
-    else
+    if @bikes.empty?
       raise "No bikes available"
+    else
+      bike = @bikes[0]
+      @bikes = @bikes.drop(1)
+      bike
     end
   end
 
   def dock(bike)
-    @bike = bike
-    @bike_counter = true
-    return @bike
+    if full? == true
+      raise "Docking station is full"
+    else
+      @bikes << bike
+    end
   end
 
+  private
+
+  def full?
+    if @bikes.size >= 20
+      return true
+    else
+      false
+    end
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,28 +4,28 @@ describe DockingStation do
   it { is_expected.to respond_to :release_bike }
     # expect(subject).to respond_to :release_bike
   # end
-  it 'get a bike' do
+  it 'gets a bike' do
+    subject.dock(Bike.new)
     expect(subject.release_bike).to be_a Bike
   end
 
   it 'checks if bike is working' do
+    subject.dock(Bike.new)
     expect(subject.release_bike.working?).to be true
   end
 
   it {is_expected.to respond_to(:dock).with(1).argument}
 
   it 'contains a bike' do
-    expect(subject).to respond_to (:bike)
+    expect(subject).to respond_to (:bikes)
   end
 
-  it 'returns docked bike' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
-  end
-
-  it 'raises error if more than one bike is taken before a bike is docked' do
-    subject.release_bike
+  it 'raises error if a bike is taken when there are no bikes' do
     expect{subject.release_bike}.to raise_error "No bikes available"
+  end
+
+  it 'tests error message if docked while docking station is full' do
+    20.times {subject.dock(Bike.new)}
+    expect{subject.dock(Bike.new)}.to raise_error "Docking station is full"
   end
 end
